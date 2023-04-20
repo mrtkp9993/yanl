0a1d93b249164710bd1b9fcdfc42e861672787cb
Murat Koptur <mkoptur3@gmail.com>
2023-04-20 19:43:04 +0300

    nelder mead

--- python/src/optim.py
+++ python/src/optim.py
@@ -37,5 +37,94 @@ def powell(f, x0, h=0.1, maxiter=100, eps=1e-6, debug=True):
     raise "Not implemented yet"
 
 
-def nelderMead(f, x0, s=0.1, maxiter=100, eps=1e-6, debug=True):
-    raise "Not implemented yet"
+def nelderMead(
+    f, x0: np.array, step=0.1, maxiter=0, noimprviter=10, eps=1e-6, debug=True
+):
+    dim = len(x0)
+    prev_best = f(x0)
+    res = [[x0, prev_best]]
+    alpha = 1
+    gamma = 2
+    rho = 0.5
+    sigma = 0.5
+    no_improv = 0
+
+    for i in range(dim):
+        x = x0.copy()
+        x[i] += step
+        score = f(x)
+        res.append([x, score])
+
+    if debug:
+        print("Initial Simplexes")
+        for r in res:
+            print(f"Simplex: {r[0]}\tScore: {r[1]:.6f}")
+
+    iters = 0
+    fevals = 2
+    while True:
+        res.sort(key=lambda x: x[1])
+        best_score = res[0][1]
+
+        if maxiter and iters >= maxiter:
+            if debug:
+                print(f"{fevals} function evalutions")
+                print(f"{iters} iterations")
+                print(f"Minimum was found at {res[0][0]}, value {res[0][1]:.6f}")
+            return res[0]
+        iters += 1
+
+        if best_score < prev_best - eps:
+            no_improv = 0
+            prev_best = best_score
+        else:
+            no_improv += 1
+
+        if no_improv > noimprviter:
+            if debug:
+                print(f"{fevals} function evalutions")
+                print(f"{iters} iterations")
+                print(f"Minimum was found at {res[0][0]}, value {res[0][1]:.6f}")
+            return res[0]
+
+        xc = [0.0] * dim
+        for t in res[:-1]:
+            for i, c in enumerate(t[0]):
+                xc[i] += c / (len(res) - 1)
+
+        xr = xc + alpha * (xc - res[-1][0])
+        rscore = f(xr)
+        fevals += 1
+        if res[0][1] <= rscore < res[-2][1]:
+            del res[-1]
+            res.append([xr, rscore])
+            continue
+
+        if rscore < res[0][1]:
+            xe = xc + gamma * (xc - res[-1][0])
+            escore = f(xe)
+            fevals += 1
+            if escore < rscore:
+                del res[-1]
+                res.append([xe, escore])
+                continue
+            else:
+                del res[-1]
+                res.append([xr, rscore])
+                continue
+
+        xco = xc + rho * (xc - res[-1][0])
+        cscore = f(xco)
+        fevals += 1
+        if cscore < res[-1][1]:
+            del res[-1]
+            res.append([xco, cscore])
+
+        xre = res[0][0]
+        nres = []
+        for t in res:
+            redx = xre + sigma * (t[0] - xre)
+            score = f(redx)
+            fevals += 1
+            nres.append([redx, score])
+        res = nres
--- python/tests/test_optim.py
+++ python/tests/test_optim.py
@@ -3,7 +3,8 @@ from unittest import TestCase
 
 sys.path.insert(0, "..")
 
-from src.optim import secant
+import numpy as np
+from src.optim import nelderMead, secant
 
 
 class Secant(TestCase):
@@ -14,3 +15,23 @@ class Secant(TestCase):
         res = secant(tf, x0, x1)
         self.assertTrue(res[2])
         self.assertAlmostEqual(res[0], -1.1053, 4)
+
+
+class NelderMead(TestCase):
+    def test_rosenbrock(self):
+        def rosenbrock(x):
+            return (x[0] - 1) ** 2 + 100 * (x[1] - x[0] ** 2) ** 2
+
+        res = nelderMead(rosenbrock, np.array([-1.2, 1.0]), step=0.2)
+        self.assertAlmostEqual(res[0][0], 1, 6)
+        self.assertAlmostEqual(res[0][1], 1, 6)
+        self.assertAlmostEqual(res[1], 0, 6)
+
+    def test_ackley(self):
+        def ackley2(x):
+            return -200 * np.exp(-0.2 * np.sqrt(x[0] ** 2 + x[1] ** 2))
+
+        res = nelderMead(ackley2, np.array([-1, 0.9]))
+        self.assertAlmostEqual(res[0][0], 0, 6)
+        self.assertAlmostEqual(res[0][1], 0, 6)
+        self.assertAlmostEqual(res[1], -200, 6)
